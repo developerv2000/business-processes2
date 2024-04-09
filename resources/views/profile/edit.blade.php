4ca44fa8de5fdbd48c78682bd8067e639a59533d
developerv2000 <developerv2000@gmail.com>
2024-04-09 15:57:28 +0500

    Added new pre content & table columns edit translations

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -13,9 +13,9 @@
         <div class="form__section">
             <h1 class="form__title main-title">{{ __('Personal data') }}</h1>
 
-            <x-form.elements.input label="{{ __('Name') }}" name="name" type="text" editing="1" :object="$user" required />
-            <x-form.elements.input label="{{ __('Email address') }}" name="email" type="email" editing="1" :object="$user" required />
-            <x-form.elements.input label="{{ __('Photo') }}" name="photo" type="file" accept=".png, .jpg, .jpeg" />
+            <x-form.grouped-elements.input label="{{ __('Name') }}" name="name" type="text" editing="1" :object="$user" required />
+            <x-form.grouped-elements.input label="{{ __('Email address') }}" name="email" type="email" editing="1" :object="$user" required />
+            <x-form.grouped-elements.input label="{{ __('Photo') }}" name="photo" type="file" accept=".png, .jpg, .jpeg" />
         </div>
     </x-form.edit-template>
 
@@ -27,8 +27,8 @@
         <div class="form__section">
             <h1 class="form__title main-title">{{ __('Password') }}</h1>
 
-            <x-form.elements.input label="{{ __('Current password') }}" name="current_password" type="password" autocomplete="current-password" minlength="4" required />
-            <x-form.elements.input label="{{ __('New password') }}" name="new_password" type="password" autocomplete="new_password" minlength="4" required />
+            <x-form.grouped-elements.input label="{{ __('Current password') }}" name="current_password" type="password" autocomplete="current-password" minlength="4" required />
+            <x-form.grouped-elements.input label="{{ __('New password') }}" name="new_password" type="password" autocomplete="new_password" minlength="4" required />
         </div>
 
         <x-different.button class="form__submit" type="submit">{{ __('Update') }}</x-different.button>

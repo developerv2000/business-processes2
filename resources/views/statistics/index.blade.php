8646c7ab582ab0512ebd8a220d4240bc2158459c
developerv2000 <developerv2000@gmail.com>
2024-05-08 16:18:13 +0500

    Statistics index page counter added

--- resources/views/statistics/index.blade.php
+++ resources/views/statistics/index.blade.php
@@ -2,6 +2,7 @@
 
 @section('main')
     <div class="main__conent-box">
+        @include('statistics.partials.counter')
         @include('statistics.tables.current-statusses-table')
         @include('statistics.tables.transitional-statusses-table')
     </div>
---
+++ resources/views/statistics/partials/counter.blade.php
@@ -0,0 +1,8 @@
+<div class="statistics-counter">
+    @foreach ($generalStatuses as $status)
+        <div class="statistics-counter__item">
+            <p class="statistics-counter__count general-status-{{ $status->id }}">{{ $status->total_current_processes_count }}</p>
+            <p class="statistics-counter__status-name">{{ $status->name }}</p>
+        </div>
+    @endforeach
+</div>
